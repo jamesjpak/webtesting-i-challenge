b3a6fac7e8862586ed59b5a1a6929e39c7541aa0
James Pak <jamespak@Josephs-MacBook-Air.local>
2019-06-17 16:01:27 -0700

    repair and success tested, working on fail function

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,15 +6,36 @@ module.exports = {
 };
 
 function succeed(item) {
-  return { ...item };
+
+  // if enhancement value is > 20, no increment
+  // if enhancement value is < 20, increment by 1
+
+    if (item.enhancement < 20) {
+      item.enhancement++
+    } 
+      
+  return { ...item }
 }
 
 function fail(item) {
+  // when enhancement less than 15 decreases durability by 5
+  // when enhancement is 15 or more durability decrease by 10
+  // when enhancement is greater than 16, enhancement decrements by one
+
+  if (item.enhancement < 15) {
+    item.durability = item.durability - 5;
+  } else if (item.enhancement > 15) {
+    item.durability = item.durability - 10
+  } else if (item.enhancement > 16) {
+    item.enhancement = item.enhancement - 1
+  }
+
   return { ...item };
 }
 
 function repair(item) {
-  return { ...item };
+  
+  return { ...item, durability: 100 };
 }
 
 function get(item) {
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,32 @@
-const enhancer = require('./enhancer.js');
+const { succeed, fail, repair, get } = require('./enhancer');
 // test away!
+
+describe('enhancer.js', () => {
+
+    describe('repair()', () => {
+        it('restores durability to 100', () => {
+
+            expect(repair({ durability: 100 }).durability).toBe(100);
+            expect(repair({ durability: 127 }).durability).toBe(100);
+            expect(repair({ durability: 72 }).durability).toBe(100);
+        })
+    })
+
+    describe('success()', () => {
+        it('increments one if level is not 20', () => {
+
+            expect(succeed({ enhancement: 7 }).enhancement).toBe(8);
+        })
+    })
+
+    describe('fail()', () => {
+        it('when enhancement less than 15 decreases durability by 5, when enhancement is 15 or more durability decrease by 10, when enhancement is greater than 16, enhancement decrements by one', () => {
+
+            expect(fail({ enhancement: 13, durability: 82 }).durability).toBe(77)
+            expect(fail({ enhancement: 17, durability: 88 }).durability).toBe(78)
+            expect(fail({ enhancement: 17, durability: 88 }).enhancement).toBe(16)
+        })
+    })
+    
+    it.todo('should have a max of 100 for durability')
+})
